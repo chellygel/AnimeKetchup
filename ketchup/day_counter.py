20d1bd87451f3e8a593c797a0eab2a4d3cf95a5e
Chelsea Winfree <chelsea@antimatter.io>
2023-08-04 16:04:17 -0400

    Major cleanup
    Added testing, tox, moved things into directories.
    Also cleaning some items up.

--- ketchup/day_counter.py
+++ ketchup/day_counter.py
@@ -12,12 +12,8 @@ def calculate_days_delta(end_date, start_date=None):
     # Calculate to datetime object
     end_date = dt.date(end_date.year, end_date.month, end_date.day)
 
-    # Delta between now and then
-    if start_date is not None:
-        start_date = dt.date(start_date.year, start_date.month, start_date.day)
-        days_delta = end_date - start_date
-    else:
-        start_date = dt.date.today()
-        days_delta = end_date - start_date
+    start_date = start_date or dt.date.today()
+    days_delta = end_date - start_date
+
     # Return a number of days
     return days_delta.days
--- ketchup/fun_fact.py
+++ ketchup/fun_fact.py
@@ -1,12 +1,8 @@
 import random as r
 
-SHOWS = {
-    "One Piece": 1070,
-    "Utena": 39
-}
+SHOWS = {"One Piece": 1070, "Utena": 39}
 
 
 def random_factoid():
     show_name, show_episodes = r.choice(list(SHOWS.items()))
     return show_name, show_episodes
-
